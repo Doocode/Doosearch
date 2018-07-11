a34846ad57dde6b6358d93203013bd3984b3b1cc
Aero15 <aero15@outlook.com>
2018-07-11 10:40:44 +0400

    Légère restructuration de fichier

--- src/search.php
+++ src/search.php
@@ -30,7 +30,19 @@
 		
 		<div class="central">
 			<div class="aligner"></div>
-			<?php include("res/php/search.php"); ?> <!-- L'application principale -->
+            
+			<!-- L'application principale -->
+            <div class="content">
+                <ul class="selectedMotors"></ul> <!-- Liste des moteurs séléctionné : pour la recherche groupé -->
+
+                <br />
+
+                <div id="form">
+                    <input type="text" id="field" name="q" placeholder="Tapez votre recherche ici" onmouseover="showTooltip('Tapez ici votre recherche');" onmouseout="showTooltip('');" spellcheck="false" autofocus />
+                    <button id="clr" onclick="$('#field').val('');" onmouseover="showTooltip('Effacer la zone de recherche');" onmouseout="showTooltip('');"><img src="res/img/close.png" /></button>
+                    <button onclick="validateForm();" onmouseover="showTooltip('Lancer la recherche');" onmouseout="showTooltip('');"><img src="res/img/find.png" /></button>
+                </div>
+            </div>
 		</div>
 		
 		<!-- Liste des sites web épinglés -->
