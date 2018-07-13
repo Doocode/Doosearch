023274cc6aa2f2058080359545c32e879d136b25
Aero15 <aero15@outlook.com>
2018-07-13 21:39:48 +0400

    Redesign de la page 'A propos' en responsive

--- src/about.php
+++ src/about.php
@@ -12,12 +12,12 @@
         <script>setCurrentPage('#aboutPage');</script>
 		
 		<div class="presentation" style="background-image: url(res/img/about.png);">
-			<h1>A propos de Doosearch</h1>
+			<h1>A propos</h1>
 		</div>
 		
 		<div class="page">
-			<img id="pub" src="res/img/ident.png" style="margin-top: -50px; border-radius: 10px 10px 0px 0px;" />
-			<h2 style="font-weight: normal; color: white; background: rgb(255,100,0); padding: 30px 40px;">Doosearch 1.3 Beta de Doocode</h2>
+			<img id="pub" src="res/img/ident.png" />
+			<h2 id="version">Doosearch 1.3 Beta de Doocode</h2>
 			<p>Le projet Doosearch consiste en un site web de fournir une expérience plus agréable aux utilisateurs en créant des outils performants et utiles basés sur des technologies du web comme le HTML 5 ou le JavaScript.</p>
 			<img src="res/img/licence-logo.png" id="licence" />
 			<p>Le projet Doosearch est un produit de <a href="http://doocode.xyz/">Doocode</a> et est placé sous la licence GPL v.3 dont vous pouvez trouver plus d'informations à cette adresse : <a href="https://www.gnu.org/licenses/gpl.html">https://www.gnu.org/licenses/gpl.html [EN]</a>.</p>
