edef68e0c1d55aa62abe69c08d7bb12682e6d4e7
Aero15 <aero15@outlook.com>
2018-07-13 17:48:27 +0400

    La touche Echap ferme les popups ouverts

---
+++ src/res/js/hub.js
@@ -0,0 +1,29 @@
+$(function(){
+    $('body').keypress(function(e){
+        var keycode = (e.keyCode ? e.keyCode : e.which);
+        if(keycode == 27) // Si la touche 'echap' est appuyé
+        {
+            if(typeof listSearchEngines !== 'undefined') // Si les moteurs sont utilisés sur la page
+            {
+                if($('.panel').css('display')!='none')
+                    showMotors();
+                if($('.menuEngine').css('display')!='none')
+                    hideMenuEngine();
+            }
+            if(typeof closeWindow !== 'undefined') // Si la fonction closeWindow existe (alors ça signifie qu'on est dans la page quickaccess.php)
+            {
+                closeWindow('#addWebsite');
+                closeWindow('#editWebsite');
+                resetForm();
+            }
+            if(typeof showEditor !== 'undefined') // Si la fonction showColorSelector existe (alors ça signifie qu'on est dans la page config.php)
+            {
+                if($('#editBgImg').css('display')!='none')
+                    showEditor('#editBgImg');
+            }
+            if(typeof showColorSelector !== 'undefined') // Si la fonction showColorSelector existe (alors ça signifie qu'on est dans la page config.php ou setup.php)
+                showColorSelector(false);
+
+        }
+    });
+});
--- src/res/php/head-hub.php
+++ src/res/php/head-hub.php
@@ -1,2 +1,3 @@
 <link rel="stylesheet" href="res/css/hub-header.css" />
 <link rel="stylesheet" href="res/css/hub.css" />
+<script src="res/js/hub.js"></script>
