236547346f9aa624b0edac269d01c6c710559005
Aero15 <aero15@outlook.com>
2018-07-14 18:37:33 +0400

    Séparation du selecteur d'image dans la page de config

---
+++ src/res/php/bgimg-selector.php
@@ -0,0 +1,32 @@
+<div class="colorEditor" id="editBgImg">
+    <div class="titleBar">
+        <button onclick="showEditor('#editBgImg');"><img src="res/img/back.png" /></button>
+        <p>Image de fond</p>
+    </div>
+    <ul class="tabs">
+        <li class="current" id="tabDefaultImg" onclick="$('#editBgImg #defaultBgImg').slideDown(); $('#editBgImg #customBgImg').slideUp(); $('#tabDefaultImg').addClass('current'); $('#tabCustomImg').removeClass('current');">Fonds d'écran</li>
+        <li id="tabCustomImg" onclick="$('#editBgImg #defaultBgImg').slideUp(); $('#editBgImg #customBgImg').slideDown(); $('#tabDefaultImg').removeClass('current'); $('#tabCustomImg').addClass('current');">Image personnalisée</li>
+    </ul>
+    <ul class="colorSelector" id="defaultBgImg">
+        <li onclick="resetBgImg();" style="background-image: url(res/img/bgs/empty.png);"></li>
+        <li onclick="setBgImg('res/img/bgs/bg1.png');" style="background-image: url(res/img/bgs/bg1.png);"></li>
+        <li onclick="setBgImg('res/img/bgs/bg2.png');" style="background-image: url(res/img/bgs/bg2.png);"></li>
+        <li onclick="setBgImg('res/img/bgs/bg3.png');" style="background-image: url(res/img/bgs/bg3.png);"></li>
+        <li onclick="setBgImg('res/img/bgs/bg4.png');" style="background-image: url(res/img/bgs/bg4.png);"></li>
+        <li onclick="setBgImg('res/img/bgs/DCG30.png');" style="background-image: url(res/img/bgs/DCG30.png);"></li>
+        <li onclick="setBgImg('res/img/bgs/DCG29.png');" style="background-image: url(res/img/bgs/DCG29.png);"></li>
+        <li onclick="setBgImg('res/img/bgs/bg6.png');" style="background-image: url(res/img/bgs/bg6.png);"></li>
+        <br/>
+        <li onclick="setBgImg('res/img/bgs/bg5.png');" style="background-image: url(res/img/bgs/bg5.png);"></li>
+        <li onclick="setBgImg('res/img/bgs/DCG28.png');" style="background-image: url(res/img/bgs/DCG28.png);"></li>
+        <li onclick="setBgImg('res/img/bgs/DCG27.png');" style="background-image: url(res/img/bgs/DCG27.png);"></li>
+        <li onclick="setBgImg('res/img/bgs/DCG25.png');" style="background-image: url(res/img/bgs/DCG25.png);"></li>
+        <li onclick="setBgImg('res/img/bgs/DCG24.png');" style="background-image: url(res/img/bgs/DCG24.png);"></li>
+        <li onclick="setBgImg('res/img/bgs/DCG23.png');" style="background-image: url(res/img/bgs/DCG23.png);"></li>
+        <li onclick="setBgImg('res/img/bgs/DCG26.png');" style="background-image: url(res/img/bgs/DCG26.png);"></li>
+        <li onclick="setBgImg('res/img/bgs/bg7.png');" style="background-image: url(res/img/bgs/bg7.png);"></li>
+    </ul>
+    <ul class="colorSelector" id="customBgImg">
+        <li id="btnImportImg" onclick="importImage();" style="background-image: url(res/img/bgs/import.png);"></li>
+    </ul>
+</div>
