dcdfff845631d987d5310dcedefb8e822baf702b
Aero15 <aero15@outlook.com>
2018-07-24 20:34:28 +0300

    Modifications dans about.php
    -Séparation du numéro de version
    -Reduction de la taille des images

--- src/about.php
+++ src/about.php
@@ -17,7 +17,8 @@
 		
 		<div class="page">
 			<img id="pub" src="res/img/ident.png" />
-			<h2 id="version">Doosearch 1.3 de Doocode</h2>
+            <h1>A propos de Doosearch</h1>
+            <h2 id="version">Version 1.3</h2>
 			<p>Le projet Doosearch consiste en un site web de fournir une expérience plus agréable aux utilisateurs en créant des outils performants et utiles basés sur des technologies du web comme le HTML 5 ou le JavaScript.</p>
 			<img src="res/img/licence-logo.png" id="licence" />
 			<p>Le projet Doosearch est un produit de <a href="http://doocode.xyz/">Doocode</a> et est placé sous la licence GPL v.3 dont vous pouvez trouver plus d'informations à cette adresse : <a href="https://www.gnu.org/licenses/gpl.html">https://www.gnu.org/licenses/gpl.html [EN]</a>.</p>
