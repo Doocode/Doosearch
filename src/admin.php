88aae43b57a52949159f6a9c81ae31cb52765a72
Aero15 <aero15@outlook.com>
2018-11-10 15:02:19 +0100

    [Admin] Routage modulable depuis admin.php

--- src/admin.php
+++ src/admin.php
@@ -26,13 +26,22 @@ else
         $matches = array();
         preg_match($regex, $_GET['page'], $matches);
         $action = $matches[1];
+        $section = $matches[2];
+        
+        $controller = '';
+        switch($section)
+        {
+            case 'search-engine':
+                $controller = 'Admin\SearchEngine';
+                break;
+        }
         
         switch($action)
         {
             case 'add':
-                $status = SearchEngine::execute($action, $_POST);
+                $status = $controller::execute($action, $_POST);
                 $data = serialize($status);
-                header('Location: admin-list-search-engine.php?status='.urlencode($data));
+                header('Location: admin-list-'.$section.'.php?status='.urlencode($data));
                 break;
             case 'edit':
                 include('res/views/admin/' . $matches[2] . '/' . $action . '.php');
@@ -43,11 +52,11 @@ else
             case 'disable':
                 $status;
                 if(isset($_GET['id']))
-                    $status = SearchEngine::execute($action, $_GET);
+                    $status = $controller::execute($action, $_GET);
                 else if(isset($_POST['id']))
-                    $status = SearchEngine::execute($action, $_POST);
+                    $status = $controller::execute($action, $_POST);
                 $data = serialize($status);
-                header('Location: admin-list-search-engine.php?status='.urlencode($data));
+                header('Location: admin-list-'.$section.'.php?status='.urlencode($data));
                 break;
             default:
                 include('res/views/admin/' . $matches[2] . '/' . $action . '.php');
