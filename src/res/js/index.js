95d4f22eb2a472cf8f486291c0cc70ec01c9863c
Aero15 <aero15@outlook.com>
2019-02-02 16:50:39 +0100

    Fix: Home page not showing "hub pages"

--- src/res/js/index.js
+++ src/res/js/index.js
@@ -1,7 +1,7 @@
 $(function(){
-	if (localStorage['doosearchVersion'] != null || localStorage['doosearchVersion'] == 1.33)
+	if (localStorage['doosearchVersion'] != null || localStorage['doosearchVersion'] >= 1.33)
     {
 		$('#btnSetup').css('display','none');
-		$('#actions #btnSearch, #actions #btnQuickAccess, #actions #btnConfig').css('display','inline-block');
+		$('.actions #btnSearch, .actions #btnQuickAccess, .actions #btnConfig').css('display','inline-block');
     }
 });
